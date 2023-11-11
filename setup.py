f5e25f4a53873240109ec39f553124af50972e85
Franco <107090061+seaborg1@users.noreply.github.com>
2023-11-11 12:53:15 -0800

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,6 @@
+
+# Copy and paste your OpenAI API key, as well as the model you want to use
+# Change "model_select" to whatever your desired model is. Such as gpt-4 (if available), gpt-3.5-turbo, etc.
+
+user_key = "Paste your API Key HERE!"
+model_select = "gpt-4"
